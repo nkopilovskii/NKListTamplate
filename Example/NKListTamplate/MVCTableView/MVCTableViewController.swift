e9dc55695bf2ee99d5b09e0895e36a397bc25f42
Nick Kopilovskii <nkopilovskii@gmail.com>
2019-06-27 18:56:31 +0300

    tmp

---
+++ Example/NKListTamplate/MVCTableView/MVCTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MVCTableViewController.swift
+//  NKListTamplate_Example
+//
+//  Created by Nick Kopilovskii on 6/27/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import NKListTamplate
+
+class MVCTableViewController: UIViewController {
+//  var tableView: UITableView
+//
+//  var tableViewConfigurator: NKTableViewConfigurator? {
+//    return nil
+//  }
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+}
+
+extension MVCTableViewController: NKTableViewDataSource {
+//  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//    <#code#>
+//  }
+//  
+//  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//    <#code#>
+//  }
+  
+//  var tableView: UITableView {
+//    return UITableView()
+//  }
+//
+//  var tableViewConfigurator: NKTableViewConfigurator? {
+//    return nil
+//  }
+}
